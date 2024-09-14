f5982108e082cb2403e79fae6edd0b9c7ba770a2
Riddhima22 <bhargavariddhima@gmail.com>
2024-09-14 19:18:27 +0530

    datatypes

---
+++ datatype.java
@@ -0,0 +1,21 @@
+public class datatype {
+    
+        public static void main(String[] args) {
+            boolean b=true;
+            int a=10;
+            int miniInt= Integer.MIN_VALUE;
+            int maxint= Integer.MAX_VALUE;
+            short sh= 12;
+            short minish= Short.MIN_VALUE;
+            short maxsh= Short.MAX_VALUE;
+            float f= 11.0982782776f;
+            double d= 11.0d;
+            char c='g';
+            byte b1= Byte.MIN_VALUE;
+            byte b2= Byte.MAX_VALUE;
+    
+    
+            System.out.println(miniInt +" "+ maxsh);
+            System.out.println(d);
+        }
+}
