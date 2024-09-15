914a126cf02cc3ee68bcc4eb39a26d1ae4ce4934
Riddhima22 <bhargavariddhima@gmail.com>
2024-09-15 15:40:16 +0530

    problems

---
+++ Lecture02/loops.java
@@ -0,0 +1,20 @@
+package Lecture02;
+
+public class loops {
+    public static void main(String[] args) {
+
+        
+        for(int i =10,;i>=0;i--){
+
+     System.out.println(j);
+        }
+        
+        
+
+    }
+}
+       
+    
+
+
+
---
+++ Lecture02/problems.java
@@ -0,0 +1,19 @@
+package Lecture02;
+import java.util.Scanner;
+public class problems {
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt();
+    
+    for (int i=2; i<n; i++){
+      if (n%i!=0){
+      System.out.println("Number is prime");
+      }
+    
+
+    
+    }
+    
+
+}
+}
